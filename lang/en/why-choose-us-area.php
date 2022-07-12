856fae52ca7a521cebd38a276a64a71b6efbd9aa
Steve Adrien <montheadrien@gmail.com>
2022-07-13 00:13:24 +0100

    modified lang

---
+++ lang/en/why-choose-us-area.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+
+
+    "why-choose-trip-story" => "Pourquoi rejoindre le reseau Trip Story",
+    "why-choose-our" => "Pourquoi rejoindre",
+    "trip-story-social-network" => "Le reseau Trip Story",
+    "what-we-do" => "What we do",
+    "why-choose-trip-story-text" => "Share in real time your photos, opinions, and the emotions you get from discovering new landscapes.",
+    "trip-story-community-text" => "Rencontrez des milliers de voyageurs du monde entier, et partagez vos expériences et recommandations voyage .",
+    "connected-people" => "Personnes Connectées",
+    "join-our-community" => "Join Our Community",
+    "meet-great-people" => "Meet Great People",
+    "meet-great-people-text" => "Meet Great People",
+    "discussion-forum" => "Meet Great People",
+    "discussion-forum-text" => "Meet Great People",
+    "active-groups" => "Meet Great People",
+    "active-groups-text" => "Meet Great People",
+    "discover-now" => "En Savoir Plus",
+
+
+
+
+
+];
--- lang/fr/why-choose-us-area.php
+++ lang/fr/why-choose-us-area.php
@@ -8,8 +8,15 @@ return [
     "why-choose-our" => "Pourquoi rejoindre",
     "trip-story-social-network" => "Le reseau Trip Story",
     "what-we-do" => "Ce que nous faisons",
+    "why-choose-trip-story-text" => "Partagez en temps réel vos photos, avis, et les émotions que vous suscitent la découverte de nouveaux paysages.",
     "trip-story-community-text" => "Rencontrez des milliers de voyageurs du monde entier, et partagez vos expériences et recommandations voyage .",
-    "connected-people" => "Personnes Connectées",
+    "join-our-community" => "Rejoignez nous",
+    "meet-great-people" => "Meet Great People",
+    "meet-great-people-text" => "Rencontrez les bonnes personnes et Bénéficiez d'une assistance étape par étape pour votre projet voyage",
+    "discussion-forum" => "Meet Great People",
+    "discussion-forum-text" => "Les professionnels du tourisme vous donnent leur avis",
+    "active-groups" => "Meet Great People",
+    "active-groups-text" => "Diverses professions du tourisme et chaînes volontaires, regroupements d'entreprises indépendantes, et/ou une labellisation ; les alliances technico-commerciales",
     "discover-now" => "En Savoir Plus",
 
 
